c90fc6192caca4c86bb315e08c346ad7b28b2c74
Shiqi Fang <sfang6@ncsu.edu>
2024-08-23 14:03:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from streamlit_folium import st_folium
 import plotly.express as px
 from pathlib import Path
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='STHM',
+    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+)
+
+# -----------------------------------------------------------------------------
+# Declare some useful functions.
+
 
 # Placeholder for loading necessary data
 # Replace with your actual data loading logic later
