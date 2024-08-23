0d7326c03cb18f3cd0472252546b3c23f7773e4b
Shiqi Fang <sfang6@ncsu.edu>
2024-08-23 11:49:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from pathlib import Path
 # Replace with your actual data loading logic later
 # reach_ids = pd.read_csv('reach_ids.csv')
 
-nwis_sites = pd.read_csv('https://github.com/fsq0511/STHM_NWM/blob/main/data/nwis_sites%20copy.csv')
+# nwis_sites = pd.read_csv('https://github.com/fsq0511/STHM_NWM/blob/main/data/nwis_sites%20copy.csv')
 # nwid_flowlist = pd.read_csv('nwid_flowlist.csv')
 # nwm_flow_fore = pd.read_csv('nwm_flow_fore.csv')
 
